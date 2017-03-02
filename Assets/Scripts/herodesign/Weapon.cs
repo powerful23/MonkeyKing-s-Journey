d043d9fafd51add998e7b04a54791bb63566681e
powerful23 <zhuowen@usc.edu>
2017-03-02 00:37:54 -0800

    wen
    add monkeyking character version 2

---
+++ Assets/Scripts/herodesign/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	public Rigidbody2D bullet;				// Prefab of the rocket.
+	public float speed = 20f;				// The speed the rocket will fire at.
+
+	private MonkeyControl playerCtrl;		// Reference to the PlayerControl script.
+	private Animator anim;					// Reference to the Animator component.
+
+	void Awake()
+	{
+		// Setting up the references.
+		anim = transform.parent.gameObject.GetComponent<Animator>();
+		playerCtrl = transform.parent.GetComponent<MonkeyControl> ();
+	}
+
+	void Update ()
+	{
+		// If the fire button is pressed...
+		if(Input.GetButtonDown("Fire1"))
+		{
+			// ... set the animator Shoot trigger parameter and play the audioclip.
+
+
+			anim.SetTrigger("Shoot");
+
+			// If the player is facing right...
+			if(playerCtrl.facingRight)
+			{
+				// ... instantiate the rocket facing right and set it's velocity to the right. 
+				Rigidbody2D bulletInstance = Instantiate(bullet, transform.GetChild(0).position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+				bulletInstance.velocity = new Vector2(speed, 0);
+
+
+			}
+			else
+			{
+				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
+				Rigidbody2D bulletInstance = Instantiate(bullet, transform.GetChild(0).position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
+				bulletInstance.velocity = new Vector2(-speed, 0);
+
+			}
+		}
+	}
+}
+
--- Assets/Scripts/mine/MyBullet.cs
+++ Assets/Scripts/mine/MyBullet.cs
@@ -5,7 +5,7 @@ public class MyBullet : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Destroy(gameObject, 10);
+		Destroy(gameObject, 3);
 	}
 	
 	// Update is called once per frame
